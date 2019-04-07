d0c1578d778b7a06afc9c489e15bbe99db9e9d3c
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 22:02:18 +0000

    Snapshot of branch master taken on 2019-04-07T18:02:17.217-04:00 from "https://github.com/UnofficialJuliaMirror/HMMBase.jl-b2b3ca75-8444-5ffa-85e6-af70e2b64fe7"

---
+++ src/HMMBase.jl
@@ -0,0 +1,55 @@
+__precompile__()
+
+"""
+Hidden Markov Models for Julia.
+"""
+module HMMBase
+
+using Distributions
+using StaticArrays
+
+import Base: rand, size
+import LinearAlgebra: mul!, transpose
+import StatsFuns: logsumexp
+
+export
+    # hmm.jl
+    AbstractHMM,
+    HMM,
+    StaticHMM,
+    assert_hmm,
+    rand,
+    # messages.jl
+    messages_backwards,
+    messages_backwards_log,
+    messages_forwards,
+    messages_forwards_log,
+    forward_backward,
+    # mle.jl
+    mle_step,
+    fit_mle,
+    fit_mle!,
+    # viterbi.jl
+    viterbi,
+    # utils.jl,
+    compute_transition_matrix
+
+# TEMP: https://github.com/JuliaStats/Distributions.jl/issues/812
+# TODO: Specify minimum Distributions version in compat
+function Distributions.allnonneg(x::AbstractArray{T}) where T<:Real
+    for i = 1 : length(x)
+        if !(x[i] >= zero(T))
+            return false
+        end
+    end
+    return true
+end
+Distributions.isprobvec(p::AbstractVector{T}) where {T<:Real} = Distributions.allnonneg(p) && isapprox(sum(p), one(T))
+
+include("hmm.jl")
+include("messages.jl")
+include("mle.jl")
+include("viterbi.jl")
+include("utilities.jl")
+
+end
